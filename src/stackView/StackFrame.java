a45b6eb7fb9fafbc3705b3c6db69b17ac4fc063e
ForrestThacker <fyruskasai@gmail.com>
2016-05-24 13:25:20 -0600

    Added to StackeFrame

--- src/stackView/StackFrame.java
+++ src/stackView/StackFrame.java
@@ -1,5 +1,32 @@
 package stackView;
+import javax.swing.*;
+import java.*;
+import java.awt.*;
+import stackController.*;
 
-public class StackFrame {
+public class StackFrame extends JFrame
+{
+	private StackController baseController;
+	private StackPanel basePanel;
+	public StackFrame(StackController drawController)
+	{
+		this.baseController = drawController;
+		basePanel = new StackPanel();
+		setUpFrame();
+	}
 
+	private void setUpFrame() 
+	{
+		this.setContentPanel(basePanel);
+		this.setResizable(true);
+		this.setTitle("Stack Dish Machine");
+		this.setSize(400, 400);
+		this.setVisible(true);
+		
+	}
+	
+	public StackController getBaseController()
+	{
+		return baseController;
+	}
 }
